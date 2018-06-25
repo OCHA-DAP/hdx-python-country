387513e6091d6a5e950d22398dde4c1c90fedab3
Mike <rans@email.com>
2018-06-25 14:24:36 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def get_readme():
     return readme_file.read()
 
 
-requirements = ['hdx-python-utilities>=1.3.6']
+requirements = ['hdx-python-utilities>=1.3.7']
 
 classifiers = [
     "Development Status :: 5 - Production/Stable",
