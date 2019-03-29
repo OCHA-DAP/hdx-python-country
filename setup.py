e86a0b5f182a5d010c4cd7faa36a213cfbcc01f6
Mike <rans@email.com>
2019-03-29 17:35:22 +0100

    Update utils

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ def get_readme():
     return readme_file.read()
 
 
-requirements = ['hdx-python-utilities>=1.6.8',
+requirements = ['hdx-python-utilities>=1.6.9',
                 'libhxl==4.5.1;python_version<"3"',
                 'libhxl>=4.13.2:python_version>="3"']
 
