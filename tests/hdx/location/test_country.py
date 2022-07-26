cb9eae5e345f4632727ad86e705ca8354d539bdb
Mike <rans@un.org>
2022-07-26 15:52:05 +1200

    Prevent Bassas Da India match to India

--- tests/hdx/location/test_country.py
+++ tests/hdx/location/test_country.py
@@ -597,6 +597,10 @@ class TestCountry:
         assert Country.get_iso3_country_code_fuzzy("Taiwan*") == ("TWN", False)
         assert Country.get_iso3_country_code_fuzzy("Kosovo") == (None, False)
         assert Country.get_iso3_country_code_fuzzy("Kosovo*") == (None, False)
+        assert Country.get_iso3_country_code_fuzzy("India") == ("IND", True)
+        assert Country.get_iso3_country_code_fuzzy("India*") == ("IND", False)
+        assert Country.get_iso3_country_code_fuzzy("*India") == ("IND", False)
+        assert Country.get_iso3_country_code_fuzzy("Bassas Da India") == (None, False)
         with pytest.raises(ValueError):
             Country.get_iso3_country_code("abc", exception=ValueError)
         with pytest.raises(ValueError):
