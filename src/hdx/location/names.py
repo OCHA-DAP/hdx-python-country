ab2a65dc43357dcde1f9968bb45b165bf12d75af
mcarans <rans@email.com>
2024-06-27 10:41:09 +1200

    Replace clean pt 1

--- src/hdx/location/names.py
+++ src/hdx/location/names.py
@@ -1,44 +1,41 @@
-import re
+import string
 import unicodedata
 
-from unidecode import unidecode
 
-non_ascii = r"([^\x00-\x7f])+"
+def make_replace_mapping():
+    chars_to_replace = [string.ascii_uppercase]
+    replacement_chars = [string.ascii_lowercase]
+    #    punctuation = string.punctuation.replace("'", "")
+    #    chars_to_replace.append(punctuation)
+    #    replacement_chars.append(" " * len(punctuation))
+    #    chars_to_replace.append(string.whitespace)
+    #    replacement_chars.append(" " * len(string.whitespace))
+
+    chars_to_replace = "".join(chars_to_replace)
+    replacement_chars = "".join(replacement_chars)
+    return str.maketrans(chars_to_replace, replacement_chars)  # , "'")
 
 
-def remove_whitespace(name: str) -> str:
-    """
-    Remove whitespace from a name.
-
-    Args:
-        name (str): Name from which to remove whitespace.
-
-    Returns:
-        str: Name without spaces
-    """
-    return "".join(name.split())
+replacement_mapping = make_replace_mapping()
 
 
 def clean_name(name: str) -> str:
     """
     Replace accented characters with non-accented ones in given name. Also
-    strip whitespace from start and end and make uppercase.
+    strip whitespace from start and end and make lowercase.
 
     Args:
-        name (str): Name to clean
+        name (str): Name to change
 
     Returns:
         str: Name without accented characters
     """
-    #
+    # Replace all non-ASCII characters with equivalent if available or remove
     clean_name = "".join(
         c
         for c in unicodedata.normalize("NFD", name)
         if unicodedata.category(c) != "Mn"
     )
-    # Remove all non-ASCII characters
-    clean_name = re.sub(non_ascii, " ", clean_name)
-    clean_name = re.sub(r"'+", "", clean_name)
-    clean_name = re.sub(r"[\W_]+", " ", clean_name)
-    clean_name = unidecode(clean_name)
-    return clean_name.strip().lower()
+    clean_name = "".join(x for x in clean_name if x in string.printable)
+    clean_name = clean_name.translate(replacement_mapping)
+    return clean_name.strip()
