03f5b9525e74280afb0c8305b11caa0373bc51f0
mcarans <rans@email.com>
2024-06-26 16:04:09 +1200

    Better cleaning

--- src/hdx/location/names.py
+++ src/hdx/location/names.py
@@ -3,16 +3,29 @@ import unicodedata
 
 from unidecode import unidecode
 
-non_ascii = "([^\x00-\x7f])+"
+non_ascii = r"([^\x00-\x7f])+"
+
+
+def remove_whitespace(name: str) -> str:
+    """
+    Remove whitespace from a name.
+
+    Args:
+        name (str): Name from which to remove whitespace.
+
+    Returns:
+        str: Name without spaces
+    """
+    return "".join(name.split())
 
 
 def clean_name(name: str) -> str:
     """
     Replace accented characters with non-accented ones in given name. Also
-    strip whitespace from start and end and make lwoercase.
+    strip whitespace from start and end and make uppercase.
 
     Args:
-        name (str): Name to change
+        name (str): Name to clean
 
     Returns:
         str: Name without accented characters
@@ -25,5 +38,7 @@ def clean_name(name: str) -> str:
     )
     # Remove all non-ASCII characters
     clean_name = re.sub(non_ascii, " ", clean_name)
+    clean_name = re.sub(r"'+", "", clean_name)
+    clean_name = re.sub(r"[\W_]+", " ", clean_name)
     clean_name = unidecode(clean_name)
     return clean_name.strip().lower()
