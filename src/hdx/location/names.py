73ddd967547d7ba55d91acb356e3f67d3f2a616d
mcarans <rans@email.com>
2024-06-27 15:00:03 +1200

    simplify and test

--- src/hdx/location/names.py
+++ src/hdx/location/names.py
@@ -18,7 +18,7 @@ def make_replace_mapping():
 
 replacement_mapping = make_replace_mapping()
 
-lowercase_space = string.ascii_lowercase + " '&/.,()-\";:"
+lowercase_space = string.ascii_lowercase + " '&/.,()-\";:" + string.digits + string.whitespace
 
 
 def clean_name(name: str) -> str:
