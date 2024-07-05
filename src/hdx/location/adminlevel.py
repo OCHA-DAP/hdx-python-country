41f843d248950e8549168835f30341b159af2364
mcarans <rans@email.com>
2024-07-05 14:28:40 +1200

    TESTING ONLY - comment fuzzy_length check

--- src/hdx/location/adminlevel.py
+++ src/hdx/location/adminlevel.py
@@ -770,7 +770,7 @@ class AdminLevel:
                     pcode = name_to_pcode.get(name.lower())
                     if pcode:
                         return pcode, True
-            if not fuzzy_match or len(name) < fuzzy_length:
+            if not fuzzy_match:  # or len(name) < fuzzy_length:
                 return None, True
             pcode = self.fuzzy_pcode(countryiso3, name, **kwargs)
             return pcode, False
