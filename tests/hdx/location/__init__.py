aad65b8051dc85eeb2a6ec65bfd6c7a1608dea1b
mcarans <rans@email.com>
2024-11-20 10:19:35 +1300

    Move Country setup from init into test class for clarity Reset urls in setup

--- tests/hdx/location/__init__.py
+++
@@ -1,8 +0,0 @@
-from hdx.location.country import Country
-
-Country.set_use_live_default(False)
-
-Country.countriesdata(
-    country_name_overrides={"PSE": "oPt"},
-    country_name_mappings={"Congo DR": "COD"},
-)
